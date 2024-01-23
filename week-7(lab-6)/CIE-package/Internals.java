c7372c0461af7d477d44823d9d8bcf828fb09c90
1BM22CS179 <154494517+1BM22CS179@users.noreply.github.com>
2024-01-23 10:41:51 +0530

    Create Internals.java

---
+++ week-7(lab-6)/CIE-package/Internals.java
@@ -0,0 +1,15 @@
+package CIE;
+
+import java.util.Scanner;
+
+public class Internals extends Student {
+    protected int[] marks = new int[5];
+
+    public void getMarks() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Internal Marks for 5 Subjects:");
+        for (int i = 0; i < 5; i++) {
+            marks[i] = scanner.nextInt();
+        }
+    }
+}
